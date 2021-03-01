af4fc6cc1004a36a68d0361b3ea4bc4a7cb0f3ea
John Chamberlin <johnchamberlin00@gmail.com>
2021-02-28 21:36:30 -0800

    config tweaks

--- api/potlucks/potlucks-model.js
+++ api/potlucks/potlucks-model.js
@@ -1,4 +1,4 @@
-const db = require('../../data/db-config')
+const db = require('../data/db-config')
 
 function find() {
 	return db('potlucks as p')
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,4 +1,4 @@
-const db = require('../../data/db-config')
+const db = require('../data/db-config')
 const Potlucks = require('../potlucks/potlucks-model')
 
 function find() {
